7edf4541dd5013ee5614a512c3f01948dd2aec5e
Mohammad Orabe <orabe.campus.tu-berlin.de>
2020-05-16 22:18:18 +0200

    Test1

---
+++ cmake-build-debug/CMakeFiles/HA1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HA1.dir/Vorgaben_Praxisteil-20200507/main.c.o"
+  "CMakeFiles/HA1.dir/Vorgaben_Praxisteil-20200507/prio_q.c.o"
+  "HA1"
+  "HA1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HA1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
